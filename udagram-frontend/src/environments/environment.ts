ecec85657ada3ae6255ef2baa21eae9ce29283e0
Tasnim Samir <tasnimsamir71@gmail.com>
2022-06-04 13:46:21 +0200

    fixing issues

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'udagram-api-dev.eba-m7rmmhm3.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://localhost:3000/api/v0'
 };
 
 /*
